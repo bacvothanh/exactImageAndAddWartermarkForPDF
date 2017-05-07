8189974033c2b00b05ba0b60e76751de6c17ab2c
Võ Thành Bắc <vothanhbac88@outlook.com.vn>
2017-05-07 17:21:29 +0700

    Add project files.

---
+++ App.Win/ExtractImage.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace App.Win
+{
+    partial class ExtractImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvertImages = new System.Windows.Forms.Button();
+            this.ofdConvertToImage = new System.Windows.Forms.OpenFileDialog();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnAddLogo = new System.Windows.Forms.Button();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvertImages
+            // 
+            this.btnConvertImages.Location = new System.Drawing.Point(55, 293);
+            this.btnConvertImages.Name = "btnConvertImages";
+            this.btnConvertImages.Size = new System.Drawing.Size(136, 33);
+            this.btnConvertImages.TabIndex = 0;
+            this.btnConvertImages.Text = "Convert to Images";
+            this.btnConvertImages.UseVisualStyleBackColor = true;
+            this.btnConvertImages.Click += new System.EventHandler(this.btnConvertImages_Click);
+            // 
+            // ofdConvertToImage
+            // 
+            this.ofdConvertToImage.FileName = "ofdConvertToImage";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(69, 64);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 13);
+            this.lbResult.TabIndex = 1;
+            // 
+            // btnAddLogo
+            // 
+            this.btnAddLogo.Location = new System.Drawing.Point(211, 293);
+            this.btnAddLogo.Name = "btnAddLogo";
+            this.btnAddLogo.Size = new System.Drawing.Size(124, 33);
+            this.btnAddLogo.TabIndex = 2;
+            this.btnAddLogo.Text = "Add logo";
+            this.btnAddLogo.UseVisualStyleBackColor = true;
+            this.btnAddLogo.Click += new System.EventHandler(this.btnAddLogo_Click);
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Location = new System.Drawing.Point(112, 13);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(100, 20);
+            this.txtOutputFolder.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output folder :";
+            // 
+            // ExtractImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 338);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.btnAddLogo);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnConvertImages);
+            this.Name = "ExtractImage";
+            this.Text = "Extract images and add watermark for PDF file";
+            this.Load += new System.EventHandler(this.ExtractImage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvertImages;
+        private System.Windows.Forms.OpenFileDialog ofdConvertToImage;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnAddLogo;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ App.Win/ExtractImage.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+
+namespace App.Win
+{
+    public partial class ExtractImage : Form
+    {
+        public ExtractImage()
+        {
+            InitializeComponent();
+        }
+
+        private void ExtractImage_Load(object sender, EventArgs e)
+        {
+            txtOutputFolder.Text = @"D:\TestResized - Copy";
+        }
+
+        private void ExtractImagesFromPdf(string sourcePdf, string outputPath)
+        {
+            // NOTE:  This will only get the first image it finds per page.
+            var pdf = new PdfReader(sourcePdf);
+            //var raf = new RandomAccessFileOrArray(sourcePdf);
+            var fileCount = 1;
+            for (var pageNumber = 1; pageNumber <= pdf.NumberOfPages; pageNumber++)
+            {
+                var pg = pdf.GetPageN(pageNumber);
+                var resource =
+                    (PdfDictionary)PdfReader.GetPdfObject(pg.Get(PdfName.RESOURCES));
+                if(resource == null) continue;
+                var xobject =
+                    (PdfDictionary)PdfReader.GetPdfObject(resource.Get(PdfName.XOBJECT));
+                if (xobject == null) continue;
+                foreach (var key in xobject.Keys)
+                {
+                    var pdfObject = xobject.Get(key);
+                    if (pdfObject.IsIndirect())
+                    {
+                        var pdfDictionary = (PdfDictionary)PdfReader.GetPdfObject(pdfObject);
+                        var type =
+                            (PdfName)PdfReader.GetPdfObject(pdfDictionary.Get(PdfName.SUBTYPE));
+                        if (PdfName.IMAGE.Equals(type))
+                        {
+                            var xrefIndex =
+                                Convert.ToInt32(
+                                    ((PRIndirectReference)pdfObject).Number.ToString(CultureInfo.InvariantCulture));
+                            var pdfObj = pdf.GetPdfObject(xrefIndex);
+                            var pdfStrem = (PdfStream) pdfObj;
+                            var bytes = PdfReader.GetStreamBytesRaw((PRStream) pdfStrem);
+                            if (bytes != null)
+                            {
+                                using (var memStream = new MemoryStream(bytes))
+                                {
+                                    memStream.Position = 0;
+                                    var img = Image.FromStream(memStream);
+                                    if (!Directory.Exists(outputPath))
+                                        Directory.CreateDirectory(outputPath);
+
+                                    var path = Path.Combine(outputPath, $@"{fileCount}.jpg");
+                                    var parms = new EncoderParameters(1)
+                                    {
+                                        Param =
+                                        {
+                                            [0] =
+                                                new EncoderParameter(
+                                                    Encoder.Compression, 0)
+                                        }
+                                    };
+                                    var jpegEncoder = GetImageEncoder("JPEG");
+                                    img.Save(path, jpegEncoder, parms);
+                                    fileCount++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            pdf.Close();
+        }
+
+        public ImageCodecInfo GetImageEncoder(string imageType)
+        {
+            imageType = imageType.ToUpperInvariant();
+            foreach (var info in ImageCodecInfo.GetImageEncoders())
+            {
+                if (info.FormatDescription == imageType)
+                {
+                    return info;
+                }
+            }
+
+            return null;
+        }
+
+        private void btnConvertImages_Click(object sender, EventArgs e)
+        {
+            lbResult.Text = string.Empty;
+            var dialogResult = ofdConvertToImage.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var fileName = ofdConvertToImage.FileName;
+                ExtractImagesFromPdf(fileName, txtOutputFolder.Text);
+            }
+        }
+
+        private void btnAddLogo_Click(object sender, EventArgs e)
+        {
+            var dialogResult = ofdConvertToImage.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                var pathFile = ofdConvertToImage.FileName;
+                var fileImageToConverts = Directory.GetFiles(txtOutputFolder.Text);
+                foreach (var fileImageToConvert in fileImageToConverts)
+                {
+                    var fileName = fileImageToConvert.Split('\\')[fileImageToConvert.Split('\\').Length - 1];
+                    var newFileName = fileName.Replace(fileName.Split('.')[0], $"{fileName.Split('.')[0]}-final");
+                    var newPathFile = fileImageToConvert.Replace(fileName, newFileName);
+
+                    using (var image = Image.FromFile(fileImageToConvert))
+                    using (var watermarkImage = Image.FromFile(pathFile))
+                    using (var imageGraphics = Graphics.FromImage(image))
+                    using (var watermarkBrush = new TextureBrush(watermarkImage))
+                    {
+                        var x = image.Width / 2 - watermarkImage.Width / 2;
+                        var y = image.Height / 2 - watermarkImage.Height / 2;
+                        watermarkBrush.TranslateTransform(x, y);
+                        imageGraphics.FillRectangle(watermarkBrush, new Rectangle(new Point(x, y), new Size(watermarkImage.Width + 1, watermarkImage.Height)));
+                        image.Save(newPathFile);
+                    }
+                }
+            }
+        }
+    }
+}
