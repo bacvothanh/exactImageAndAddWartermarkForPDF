a05c8d3b784c523716b22f6f0b577ef1b7f4a1df
Võ Thành Bắc <vothanhbac88@outlook.com.vn>
2017-05-09 22:40:54 +0700

    update resize picture

--- App.Win/ExtractImage.Designer.cs
+++ App.Win/ExtractImage.Designer.cs
@@ -34,6 +34,9 @@
             this.btnAddLogo = new System.Windows.Forms.Button();
             this.txtOutputFolder = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnAddWhiteBackground = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnConvertImages
@@ -72,7 +75,7 @@
             // 
             this.txtOutputFolder.Location = new System.Drawing.Point(112, 13);
             this.txtOutputFolder.Name = "txtOutputFolder";
-            this.txtOutputFolder.Size = new System.Drawing.Size(100, 20);
+            this.txtOutputFolder.Size = new System.Drawing.Size(209, 20);
             this.txtOutputFolder.TabIndex = 3;
             // 
             // label1
@@ -84,11 +87,40 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Output folder :";
             // 
+            // btnAddWhiteBackground
+            // 
+            this.btnAddWhiteBackground.Location = new System.Drawing.Point(361, 293);
+            this.btnAddWhiteBackground.Name = "btnAddWhiteBackground";
+            this.btnAddWhiteBackground.Size = new System.Drawing.Size(136, 33);
+            this.btnAddWhiteBackground.TabIndex = 5;
+            this.btnAddWhiteBackground.Text = "Add white background";
+            this.btnAddWhiteBackground.UseVisualStyleBackColor = true;
+            this.btnAddWhiteBackground.Click += new System.EventHandler(this.btnAddWhiteBackground_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(111, 45);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(210, 20);
+            this.txtInput.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input folder :";
+            // 
             // ExtractImage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(529, 338);
+            this.Controls.Add(this.btnAddWhiteBackground);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInput);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtOutputFolder);
             this.Controls.Add(this.btnAddLogo);
@@ -110,6 +142,9 @@
         private System.Windows.Forms.Button btnAddLogo;
         private System.Windows.Forms.TextBox txtOutputFolder;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddWhiteBackground;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- App.Win/ExtractImage.cs
+++ App.Win/ExtractImage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.Globalization;
 using System.IO;
@@ -17,7 +18,8 @@ namespace App.Win
 
         private void ExtractImage_Load(object sender, EventArgs e)
         {
-            txtOutputFolder.Text = @"D:\TestResized - Copy";
+            txtOutputFolder.Text = @"C:\Users\Admin\Desktop\Oc\output";
+            txtInput.Text = @"C:\Users\Admin\Desktop\Oc\input";
         }
 
         private void ExtractImagesFromPdf(string sourcePdf, string outputPath)
@@ -60,7 +62,7 @@ namespace App.Win
                                     if (!Directory.Exists(outputPath))
                                         Directory.CreateDirectory(outputPath);
 
-                                    var path = Path.Combine(outputPath, $@"{fileCount}.jpg");
+                                    var path = Path.Combine(outputPath, $@"{fileCount}.png");
                                     var parms = new EncoderParameters(1)
                                     {
                                         Param =
@@ -70,7 +72,7 @@ namespace App.Win
                                                     Encoder.Compression, 0)
                                         }
                                     };
-                                    var jpegEncoder = GetImageEncoder("JPEG");
+                                    var jpegEncoder = GetImageEncoder("PNG");
                                     img.Save(path, jpegEncoder, parms);
                                     fileCount++;
                                 }
@@ -113,25 +115,134 @@ namespace App.Win
             if (dialogResult == DialogResult.OK)
             {
                 var pathFile = ofdConvertToImage.FileName;
-                var fileImageToConverts = Directory.GetFiles(txtOutputFolder.Text);
+                var fileImageToConverts = Directory.GetFiles(txtInput.Text);
                 foreach (var fileImageToConvert in fileImageToConverts)
                 {
                     var fileName = fileImageToConvert.Split('\\')[fileImageToConvert.Split('\\').Length - 1];
-                    var newFileName = fileName.Replace(fileName.Split('.')[0], $"{fileName.Split('.')[0]}-final");
-                    var newPathFile = fileImageToConvert.Replace(fileName, newFileName);
+                    var newPathFile = $"{txtOutputFolder.Text}\\{fileName}";
 
                     using (var image = Image.FromFile(fileImageToConvert))
                     using (var watermarkImage = Image.FromFile(pathFile))
                     using (var imageGraphics = Graphics.FromImage(image))
-                    using (var watermarkBrush = new TextureBrush(watermarkImage))
                     {
-                        var x = image.Width / 2 - watermarkImage.Width / 2;
-                        var y = image.Height / 2 - watermarkImage.Height / 2;
-                        watermarkBrush.TranslateTransform(x, y);
-                        imageGraphics.FillRectangle(watermarkBrush, new Rectangle(new Point(x, y), new Size(watermarkImage.Width + 1, watermarkImage.Height)));
-                        image.Save(newPathFile);
+                        if (image.Width <= image.Height)
+                        {
+                            var newWidth = image.Width*90/100;
+                            double percent = (double) newWidth/(double) watermarkImage.Width;
+                            var newHeight = watermarkImage.Height*percent;
+
+                            var watermarkImageResized = ResizeImage(watermarkImage, newWidth, (int) newHeight);
+                            using (var watermarkBrush = new TextureBrush(watermarkImageResized))
+                            {
+                                var x = image.Width/2 - watermarkImageResized.Width/2;
+                                var y = image.Height/2 - watermarkImageResized.Height/2;
+                                watermarkBrush.TranslateTransform(x, y);
+                                imageGraphics.FillRectangle(watermarkBrush,
+                                    new Rectangle(new Point(x, y),
+                                        new Size(watermarkImageResized.Width + 1, watermarkImageResized.Height)));
+                                image.Save(newPathFile);
+                            }
+                        }
+                        else
+                        {
+                            var newHeight = image.Height*90/100;
+                            double percent = (double)newHeight / (double)watermarkImage.Height;
+                            var newWidth = watermarkImage.Width * percent;
+                            var watermarkImageResized = ResizeImage(watermarkImage, (int)newWidth, newHeight);
+                            using (var watermarkBrush = new TextureBrush(watermarkImageResized))
+                            {
+                                var x = image.Width / 2 - watermarkImageResized.Width / 2;
+                                var y = image.Height / 2 - watermarkImageResized.Height / 2;
+                                watermarkBrush.TranslateTransform(x, y);
+                                imageGraphics.FillRectangle(watermarkBrush,
+                                    new Rectangle(new Point(x, y),
+                                        new Size(watermarkImageResized.Width + 1, watermarkImageResized.Height)));
+                                image.Save(newPathFile);
+                            }
+                        }
+                        
                     }
                 }
+            }
+        }
+
+        private static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void btnAddWhiteBackground_Click(object sender, EventArgs e)
+        {
+            var backgroundImagePath = @"C:\Users\Admin\Desktop\Oc\background.jpg";
+            var fileImageToConverts = Directory.GetFiles(txtInput.Text);
+            foreach (var fileImageToConvert in fileImageToConverts)
+            {
+                var fileName = fileImageToConvert.Split('\\')[fileImageToConvert.Split('\\').Length - 1];
+                var newPathFile = $"{txtOutputFolder.Text}\\{fileName}";
+
+                using (var backgroundImage = Image.FromFile(backgroundImagePath))
+                using (var orinalImage = Image.FromFile(fileImageToConvert))
+                {
+                    if (orinalImage.Width >= orinalImage.Height)
+                    {
+                        var newWidth = orinalImage.Width * 140 / 100;
+                        double percent = (double)newWidth / (double)orinalImage.Width;
+                        var newHeight = orinalImage.Height * percent;
+                        var newBackgroundImage = ResizeImage(backgroundImage, newWidth, (int)newHeight);
+                        using (var imageGraphics = Graphics.FromImage(newBackgroundImage))
+                        {
+                            using (var watermarkBrush = new TextureBrush(orinalImage))
+                            {
+                                var x = newBackgroundImage.Width / 2 - orinalImage.Width / 2;
+                                var y = newBackgroundImage.Height / 2 - orinalImage.Height / 2;
+                                watermarkBrush.TranslateTransform(x, y);
+                                imageGraphics.FillRectangle(watermarkBrush,
+                                    new Rectangle(new Point(x, y),
+                                        new Size(orinalImage.Width + 1, orinalImage.Height)));
+                                newBackgroundImage.Save(newPathFile);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        var newHeight = orinalImage.Height * 140 / 100;
+                        double percent = (double)newHeight / (double)orinalImage.Height;
+                        var newWidth = orinalImage.Width * percent;
+                        var newBackgroundImage = ResizeImage(backgroundImage, (int)newWidth, newHeight);
+                        using (var imageGraphics = Graphics.FromImage(newBackgroundImage)) 
+                        using (var watermarkBrush = new TextureBrush(orinalImage))
+                        {
+                            var x = newBackgroundImage.Width / 2 - orinalImage.Width / 2;
+                            var y = newBackgroundImage.Height / 2 - orinalImage.Height / 2;
+                            watermarkBrush.TranslateTransform(x, y);
+                            imageGraphics.FillRectangle(watermarkBrush,
+                                new Rectangle(new Point(x, y),
+                                    new Size(orinalImage.Width + 1, orinalImage.Height)));
+                            newBackgroundImage.Save(newPathFile);
+                        }
+                    }
+                }
+                
             }
         }
     }
